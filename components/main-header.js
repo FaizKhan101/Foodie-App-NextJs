38ad29cd4a02735197741f62da918aa0604feadc
Faiz <faizkhan8225@gmail.com>
2024-12-14 17:24:13 +0530

    Optimize images with the nextjs Image component.

--- components/main-header.js
+++ components/main-header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import classes from "./main-header.module.css"
 import logoImg from "@/assets/logo.png"
@@ -6,7 +7,7 @@ import logoImg from "@/assets/logo.png"
 export default function MainHeader() {
     return <header className={classes.header}>
         <Link className={classes.logo} href="/">
-            <img src={logoImg.src} alt="A plate with food on it." />
+            <Image src={logoImg} alt="A plate with food on it." />
             NextLevel Food
         </Link>
 
