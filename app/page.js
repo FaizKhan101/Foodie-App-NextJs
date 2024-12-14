42edff085612eefba4ae222032941d2517b66564
Faiz <faizkhan8225@gmail.com>
2024-12-14 17:47:38 +0530

    Populating a home page content specific header

--- app/page.js
+++ app/page.js
@@ -1,15 +1,24 @@
 import Link from "next/link";
 
-export default function Home() {
-  return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p><Link href="/meals">Meals</Link></p>
-      <p><Link href="/meals/share">Meals Share</Link></p>
-      <p><Link href="/community">Community</Link></p>
+import classes from "./page.module.css"
 
-    </main>
-  );
+export default function Home() {
+  return <>
+    <header className={classes.header}>
+      <div className={classes.slideshow}>
+
+      </div>
+      <div>
+        <div className={classes.hero}>
+          <h1>NextLevel Food for NextLevel Foodies</h1>
+          <p>Taste and share food from all over the world.</p>
+        </div>
+        <div className={classes.cta}>
+          <Link href="/community">Join the Community</Link>
+          <Link href="/meals">Explore Meals</Link>
+        </div>
+      </div>
+    </header>
+    <main></main>
+  </>
 }
