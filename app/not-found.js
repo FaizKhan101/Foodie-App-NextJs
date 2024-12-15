9ed190151884818ded5040da0cabcddb1a85938e
Faiz <faizkhan8225@gmail.com>
2024-12-15 22:57:42 +0530

    Handling not found error.

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+'use client'
+
+export default function notFoundPage() {
+    return <main className="error">
+        <h1>Page Not Found!</h1>
+        <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+}
